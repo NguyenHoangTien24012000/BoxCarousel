2e9fffd3bb2ba9e28a133092de9b1f63683c6ee6
NguyenHoangTien24012000 <hoangtienflytom@gmail.com>
2022-03-18 18:10:02 +0700

    update

---
+++ src/js/BoxTuThuoc/boxTuThuoc1.js
@@ -0,0 +1,47 @@
+const carouselBox = function (selector) {
+
+    let slider = document.querySelector(`#${selector}`);
+
+    let carousel = slider.querySelector('.carousel__container');
+
+    //Kich thuoc the con + 10px margin
+    let cardWidth = slider.querySelector('.carousel__item').offsetWidth + 10;
+
+    //So luong the con
+    let numberItem = slider.querySelectorAll('.carousel__item').length;
+
+    function createNodeChildLi() {
+        for (let i = 0; i < numberItem; i++) {
+            var li = document.createElement("li");
+            li.classList.add("control__item")
+            li.setAttribute("index", i)
+            slider.querySelector('.control .control__container').appendChild(li);
+        }
+        slider.querySelectorAll('.control li')[0].classList.add('active');
+    }
+
+
+    function activeElenment() {
+        slider.querySelectorAll('.control__item').forEach((item) => {
+            item.classList.remove("active")
+        });
+    }
+
+    function carouselAction() {
+        slider.querySelectorAll('.control__item').forEach((item, index) => {
+            item.addEventListener('click', () => {
+                activeElenment()
+                item.classList.add('active')
+                if (index === numberItem - 1) {
+                    carousel.style.transform = `translateX(-${cardWidth * index - 75}px )`;
+                }else{
+                    carousel.style.transform = `translateX(-${cardWidth * index}px )`;
+                }
+             
+            });
+        });
+    }
+    return createNodeChildLi(), carouselAction();
+}
+
+carouselBox('box__tuThuoc');
